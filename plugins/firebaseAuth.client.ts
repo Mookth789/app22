6301f38de6d1e8146849e28cf3eb60fceb152fa4
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-26 18:08:35 +0700

    1 file modified, 1 added, 1 deleted

--- plugins/firebaseAuth.client.ts
+++ plugins/firebaseAuth.client.ts
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 export default defineNuxtPlugin((nuxtApp) => {
 
   const firebaseConfig = {
- 
+    apiKey: "AIzaSyCZAETq5uDKOJucoCr8GYosqgpst7Oo0DY",
 
 };
 
